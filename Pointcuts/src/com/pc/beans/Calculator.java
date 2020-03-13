097b87a40cfa3fe4e3864ab11ae35054d97dd9e6
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-03-13 11:53:03 +0530

    With Beans

---
+++ Pointcuts/src/com/pc/beans/Calculator.java
@@ -0,0 +1,12 @@
+package com.pc.beans;
+
+public class Calculator {
+	public int add(int a, int b) {
+		System.out.println(a + b);
+		return a + b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+}
